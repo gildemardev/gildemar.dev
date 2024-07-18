c43ff19d6b968e824448f2f0ca3e84f1a321cf46
GildemarDev <gxldemar@gmail.com>
2024-07-17 21:26:43 -0300

    :sparkles: add Paragraph component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,60 @@
+import Paragraph from "@/typhography/Paragraph";
+
 export default function Home() {
+	const sideProjects = [
+		"Um CRM para marketing direto.",
+		"Um sistema de cardápio online e gerenciamento de atendimentos.",
+		"Uma ferramenta de IA que pode assessorar diretamente seu atendimento e suas vendas.",
+	];
+
 	return (
 		<main className='flex min-h-screen flex-col items-center px-24'>
 			<section className='max-w-3xl'>
-				<p>Motion Designer & Fullstack Developer.</p>
-				<p>
+				<Paragraph>Motion Designer & Fullstack Developer.</Paragraph>
+				<Paragraph>
 					Especializado em micro interações, posso agregar no seu projeto.
 					Vivemos em uma geração onde a atenção do usuário é capturada nos
 					primeiros segundos de acesso . Ação e movimento resumem tudo!
-				</p>
-				<p>
+				</Paragraph>
+				<Paragraph>
 					Softwares são mais do que meros programas ou sites; eles são
 					experiências que se entrelaçam com o cotidiano dos usuários. Assim
 					como um inquilino aprecia os pequenos detalhes de seu lar alugado, os
 					usuários valorizam as nuances que tornam a interação com o software
 					única. Cada clique, deslize ou toque é uma oportunidade para encantar
 					e engajar, criando um vínculo contínuo entre o cliente e o serviço.
-				</p>
-				<p>
+				</Paragraph>
+				<Paragraph>
 					Desde 2014, trabalho na área do design como social media e motion
 					designer. Participei da transição de posts em formato de imagem, sem
 					movimento, para os vídeos curtos que dispararam em popularidade no
 					marketing. Porquê não aplicar isso ao web design?
-				</p>
-				<p>
+				</Paragraph>
+				<Paragraph>
 					Com minha experiência em motion design, trago uma perspectiva única
 					para o desenvolvimento de interfaces. Micro interações não são apenas
 					uma tendência, mas uma necessidade para se destacar no mercado digital
 					atual. Esses pequenos detalhes fazem com que os usuários se sintam
 					valorizados e proporcionam uma sensação de fluidez e continuidade na
 					navegação.
-				</p>
-				<p>
+				</Paragraph>
+				<Paragraph>
 					Se você busca transformar seu projeto em algo mais do que um simples
 					site, estou aqui para ajudar. Juntos, podemos criar experiências
 					digitais que não apenas atendem às expectativas dos usuários, mas as
 					superam. Vamos explorar o poder do motion design para converter e
 					fidelizar seus clientes.
-				</p>
-				<p>Atualmente estou trabalhando em 3 side projects:</p>
-				<ul>
-					<li>Um CRM para marketing direto.</li>
-					<li>
-						Um sistema de cardápio online e gerenciamento de atendimentos.
-					</li>
-					<li>
-						Uma ferramenta de IA que pode assessorar diretamente seu atendimento
-						e suas vendas.
-					</li>
+				</Paragraph>
+				<Paragraph>Atualmente estou trabalhando em 3 side projects:</Paragraph>
+				<ul className='text-lg space-y-2 mt-2'>
+					{sideProjects.map((sideProject, index) => {
+						return <li key={index}>{`=> ${sideProject}`}</li>;
+					})}
 				</ul>
-				<p>
+				<Paragraph>
 					Quer saber mais ou simplesmente falar comigo? Sinta-se à vontade para
 					preencher o formulário abaixo.
-				</p>
+				</Paragraph>
 			</section>
 		</main>
 	);
---
+++ src/typhography/Paragraph.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+interface ParagraphProps extends React.ComponentPropsWithoutRef<"p"> {
+	children: React.ReactNode;
+}
+
+const Paragraph = ({ children, className = "", ...props }: ParagraphProps) => {
+	return (
+		<p
+			className={cn("leading-7 mt-6 first:mt-0 text-lg", className)}
+			{...props}>
+			{children}
+		</p>
+	);
+};
+
+export default Paragraph;
