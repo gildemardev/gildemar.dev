a13e0deb302f9ae6398a6e363193bc9ae014f484
gildemardev <gxldemar@gmail.com>
2024-07-24 02:19:49 -0300

    :lipstick: apply variables colors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: FC = () => {
 	return (
 		<footer className='flex flex-col w-full max-w-3xl font-sans tracking-tight'>
 			{/* TODO: Aplicar variaveis de cor */}
-			<hr className='text-white h-1 w-full py-2.5' />
+			<hr className='border-border h-1 w-full py-2.5' />
 			<p>
 				2024. Feito por{" "}
 				<strong>
