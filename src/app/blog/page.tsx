cfdc9552cf602cdd854c7c9230a6fc75eebfc95c
gildemardev <gxldemar@gmail.com>
2024-08-18 00:44:03 -0300

    :lipstick: removed annoying padding in all posts list

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -46,7 +46,7 @@ export default function BlogPage() {
 	const posts = getPostMetadata();
 
 	return (
-		<div className='container mx-auto px-4 py-8'>
+		<div className='container mx-auto py-8'>
 			<h1 className='text-3xl font-bold mb-8'>Blog Posts</h1>
 			{posts.length > 0 ? (
 				<div className='grid grid-cols-2 gap-6'>
