e2eecbc4d701d4caa6dc9b6c68802596b4e4fc5f
gildemardev <gxldemar@gmail.com>
2024-08-19 02:14:13 -0300

    :construction: testing transition timing in prod

--- src/components/TransitionLayout.tsx
+++ src/components/TransitionLayout.tsx
@@ -1,36 +1,36 @@
 "use client";
-import { FC } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { usePathname } from "next/navigation";
 
-interface TransitionLayoutProps {
+export default function AnimatedLayout({
+	children,
+}: {
 	children: React.ReactNode;
-}
-
-const TransitionLayout: FC<TransitionLayoutProps> = ({ children }) => {
+}) {
 	const pathname = usePathname();
 
 	return (
 		<AnimatePresence mode='wait'>
 			<motion.div key={pathname} className='relative w-full'>
+				{/* Conteúdo da página */}
 				{children}
+
+				{/* Animação de transição */}
 				<motion.div
-					className='fixed inset-0 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 origin-left z-50 pointer-events-none backdrop-blur-sm'
+					className='fixed inset-0 cool-background origin-left z-50'
 					initial={{ scaleX: 0 }}
 					animate={{ scaleX: 0 }}
 					exit={{ scaleX: 1 }}
 					transition={{ duration: 0.7, ease: "easeInOut" }}
 				/>
 				<motion.div
-					className='fixed inset-0 bg-gradient-to-l from-blue-500 via-purple-500 to-pink-500 origin-right z-50 pointer-events-none backdrop-blur-sm'
+					className='fixed inset-0 cool-background origin-right z-50'
 					initial={{ scaleX: 1 }}
 					animate={{ scaleX: 0 }}
 					exit={{ scaleX: 0 }}
-					transition={{ duration: 0.7, ease: "easeInOut" }}
+					transition={{ duration: 0.5, ease: "easeInOut" }}
 				/>
 			</motion.div>
 		</AnimatePresence>
 	);
-};
-
-export default TransitionLayout;
+}
