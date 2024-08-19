67dc7e3fc753f9476fd2f18b0085c0d7390b1e23
gildemardev <gxldemar@gmail.com>
2024-08-19 01:23:06 -0300

    :dizzy: add cool hover transition on bento grid

--- src/components/BentoGrid.tsx
+++ src/components/BentoGrid.tsx
@@ -11,30 +11,30 @@ const BentoGrid: FC<BentoGridProps> = ({}) => {
 			</h3>
 
 			<div className='flex h-full w-full items-center justify-center'>
-				<div className='grid h-full w-full gap-4 grid-cols-2 md:grid-cols-4 sm:grid-rows-2 md:grid-rows-4 rounded-lg shadow-md'>
-					<div className='p-10 col-span-2 row-span-2 bg-[#4960D7] rounded-lg shadow-md flex items-center justify-center'>
+				<div className='grid h-full w-full grid-cols-2 md:grid-cols-4 sm:grid-rows-2 md:grid-rows-4 shadow-md'>
+					<div className='p-10 col-span-2 row-span-2 bg-[#4960D7] shadow-md flex items-center justify-center transition-transform duration-300 hover:scale-95'>
 						<div className='aspect-square md:aspect-auto flex justify-center items-center'>
 							<p className='text-center '>MOTION DESIGN</p>
 						</div>
 					</div>
 
-					<div className='p-10 aspect-square col-span-1 row-span-2 bg-[#d43f00] rounded-lg shadow-md flex items-center justify-center'>
+					<div className='p-10 aspect-square col-span-1 row-span-2 bg-[#d43f00] transition-transform duration-300 hover:scale-95 shadow-md flex items-center justify-center'>
 						<p className='text-center'>ÍCONES PERSONALIZADOS</p>
 					</div>
 
-					<div className='p-10 aspect-square col-span-1 row-span-2 bg-[#FCDA7F] text-black rounded-lg shadow-md flex items-center justify-center'>
+					<div className='p-10 aspect-square col-span-1 row-span-2 bg-[#FCDA7F] transition-transform duration-300 hover:scale-95 text-black shadow-md flex items-center justify-center'>
 						<p>PROJETO 1</p>
 					</div>
 
-					<div className='p-10 col-span-1 aspect-square row-span-2 bg-red-200 rounded-lg shadow-md flex items-center justify-center'>
+					<div className='p-10 col-span-1 aspect-square row-span-2 bg-red-200  transition-transform duration-300 hover:scale-95 shadow-md flex items-center justify-center'>
 						<p>PUBLICIDADE</p>
 					</div>
 
-					<div className='p-10 aspect-square col-span-1 row-span-2 bg-[#F00816] rounded-lg shadow-md flex items-center justify-center'>
+					<div className='p-10 aspect-square col-span-1 row-span-2 bg-[#F00816] transition-transform duration-300 hover:scale-95 shadow-md flex items-center justify-center'>
 						<p className='text-center'>LOGOS ANIMADOS</p>
 					</div>
 
-					<div className='p-10  aspect-video sm:aspect-auto col-span-2 row-span-2 bg-[#1ED760] rounded-lg shadow-md flex items-center justify-center'>
+					<div className='p-10  aspect-video sm:aspect-auto col-span-2 row-span-2  transition-transform duration-300 hover:scale-95 bg-[#1ED760] shadow-md flex items-center justify-center'>
 						<p>PROJETO 2</p>
 					</div>
 				</div>
