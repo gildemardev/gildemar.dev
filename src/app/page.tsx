8dc9db33d866c590a639690a01891c452c99655d
gildemardev <gxldemar@gmail.com>
2024-08-19 01:14:33 -0300

    :fire: reduces redundancy at homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,42 +35,34 @@ export default function Home() {
 				<Header />
 				<section className='max-w-3xl pb-10'>
 					{/* TODO: Enfatizar algumas frases com a tag strong */}
-					<Paragraph>Motion Designer & Fullstack Developer.</Paragraph>
+
 					<Paragraph>
-						Especializado em micro interações, posso agregar no seu projeto.
-						Vivemos em uma geração onde a atenção do usuário é capturada nos
-						primeiros segundos de acesso. Ação e movimento resumem tudo!
+						Softwares são mais do que programas ou sites, eles são experiências
+						que se entrelaçam com o cotidiano dos usuários. Assim como um
+						inquilino aprecia os detalhes de seu lar alugado, os usuários
+						valorizam as nuances que tornam a interação com o software única.
+						Cada clique é uma oportunidade para encantar e engajar, criando um
+						vínculo contínuo com o cliente.
 					</Paragraph>
 					<Paragraph>
-						Softwares são mais do que meros programas ou sites; eles são
-						experiências que se entrelaçam com o cotidiano dos usuários. Assim
-						como um inquilino aprecia os pequenos detalhes de seu lar alugado,
-						os usuários valorizam as nuances que tornam a interação com o
-						software única. Cada clique, deslize ou toque é uma oportunidade
-						para encantar e engajar, criando um vínculo contínuo entre o cliente
-						e o serviço.
-					</Paragraph>
-					<Paragraph>
-						Desde 2017, trabalhei inicialmente como social media e motion
-						designer e iniciei também na programação. Participei da transição de
-						posts em formato de imagem, sem movimento, para os vídeos curtos que
-						dispararam em popularidade no marketing. Porquê não aplicar isso ao
-						web design?
+						Desde 2017 trabalhei como Social Media, Motion Designer e aprendi
+						programação. Participei da transição do formato de marketing para
+						algo mais interativo e rápido. Porquê não aplicar isso ao web
+						design?
 					</Paragraph>
 					<Paragraph>
 						Com minha experiência trago uma perspectiva única para o
-						desenvolvimento de interfaces. Micro interações não são apenas uma
-						tendência, mas uma necessidade para se destacar no mercado digital
-						atual. Esses pequenos detalhes fazem com que os usuários se sintam
-						valorizados e proporcionam uma sensação de fluidez e continuidade na
-						navegação.
+						desenvolvimento web. Micro interações tem o poder necessário para se
+						destacar no mercado digital atual. Esses pequenos detalhes fazem com
+						que os usuários se sintam valorizados e proporcionam uma sensação de
+						fluidez e continuidade na navegação.
 					</Paragraph>
 					<Paragraph>
 						Se você busca transformar seu projeto em algo mais do que um simples
 						site, estou aqui para ajudar. Juntos, podemos criar experiências
 						digitais que não apenas atendem às expectativas dos usuários, mas as
 						superam. Vamos explorar o poder do motion para converter e fidelizar
-						seus clientes.
+						seus clientes!
 					</Paragraph>
 					<BentoGrid />
 					<Paragraph id='contact'>
